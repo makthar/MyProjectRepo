244372244c43c0192fe1480cbc2af0a4b80da6c8
bashabhasha <bashabhasha786@gmail.com>
2025-05-30 07:03:45 +0530

    updating the file

--- First.java
+++ First.java
@@ -1,2 +1,8 @@
 my first line java code
+<<<<<<< Updated upstream
 dev1 developed second line java code
+=======
+makthar secondline java code
+makthar thirdline java code
+
+>>>>>>> Stashed changes
